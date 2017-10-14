249b9949aecb9a0d5bbb4dbd63576adebedb37a0
Mohit Yadav <f2014074@pilani.bits-pilani.ac.in>
2017-10-14 19:42:57 +0530

    Added all files

---
+++ map_merger/src/mapstitch.h
@@ -0,0 +1,43 @@
+#ifndef MAPSTITCH_H
+#define MAPSTITCH_H
+
+#include <stdio.h>
+#include <math.h>
+#include <opencv/cv.h>
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/video/tracking.hpp"
+// #include "opencv2/legacy/legacy.hpp"
+using namespace cv;
+using namespace std;
+
+class StitchedMap
+{
+public:
+  StitchedMap(Mat &img1, Mat &img2,int max_trans,
+              int max_rotation,float max_pairwise_distance,
+              cv::Mat oldTransform);
+  ~StitchedMap();
+  bool works;
+  float mid_distance;
+  Mat get_debug();
+  Mat get_stitch();
+  Mat H; // transformation matrix
+  Mat image1, image2,
+      dscv1, dscv2;
+  vector<KeyPoint> kpv1,kpv2;
+  vector<KeyPoint> fil1,fil2;
+  vector<Point2f>  coord1,coord2;
+  vector<DMatch>   matches;
+  vector<Rect> rects1,rects2,rects3 ;
+  double rotation,transx,transy,scalex,scaley;
+
+  cv::Mat cur_trans;
+
+};
+
+#endif // MAPSTITCH_H
